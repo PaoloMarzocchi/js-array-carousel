5f98036e9659564f51941c95910ff1d69c82b273
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-23 17:26:00 +0100

    add dynamic carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,52 @@ const slider = document.querySelector('.carousel');
 let activeImg = 0;
     //    -ciclo per scorrere nell'array d'immagini e sostituire sequenzialmente la classe active
 
+
 for (let i = 0; i < imgList.length; i++) {
     const img = imgList[i];
     //console.log(img);
-    imgMarkup = `<img class="${i === activeImg ? "active" : ""}" src="./assets/img/${img}" alt=""></img>`;
+    let imgMarkup = `<img class="${i === activeImg ? "active" : ""}" src="./assets/img/${img}" alt=""></img>`;
     //console.log(imgMarkup);
-    slider.insertAdjacentHTML('beforeend', imgMarkup)
+    slider.insertAdjacentHTML('beforeend', imgMarkup);
 }
 
-
-
-
 // - aggiungo gli eventi ai click dei due bottoni (avanti e indietro)
+const up = document.querySelector('.up');
+const down = document.querySelector('.down');
+
+up.addEventListener('click', function(){
+    activeImg++;
+
+    if (activeImg > imgList.length -1) {
+        activeImg = 0;
+    }
+    //console.log(activeImg);
+    //console.log(imgList[activeImg]);
+    let imgElement = document.querySelector('img.active');
+    //console.log(imgElement);
+    imgElement.classList.remove("active");
+    //console.log(imgElement.classList);
+    let allImg = document.querySelectorAll('.carousel img');
+    //console.log(allImg);
+    allImg[activeImg].classList.add("active");
+
+})
+
+down.addEventListener('click', function(){
+    activeImg--;
+    if (activeImg < 0) {
+        activeImg = imgList.length -1;
+    }
+    //console.log(activeImg);
+    //console.log(imgList[activeImg]);
+    let imgElement = document.querySelector('img.active');
+    //console.log(imgElement);
+    imgElement.classList.remove("active");
+    //console.log(imgElement.classList);
+    let allImg = document.querySelectorAll('.carousel img');
+    //console.log(allImg);
+    allImg[activeImg].classList.add("active");
+})
+
  //    -al click aumento di 1 (o diminuisco di 1) la variabile segnaposto
  //    -seleziono l'immagine successiva andando a cambiare il percorso tramite l'indice dell'array che sarà uguale alla variabile segnaposto
