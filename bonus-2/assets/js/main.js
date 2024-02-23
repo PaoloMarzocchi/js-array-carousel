80778bd72e6de1965641aef4f54f57dc81b1785f
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-23 19:20:11 +0100

    add shadow on unselected images

--- bonus-2/assets/js/main.js
+++ bonus-2/assets/js/main.js
@@ -4,18 +4,24 @@
 const imgList = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg'];
     //    -seleziono il tag che contiene le immagini
 const slider = document.querySelector('.current_img');
+const thumbElement = document.querySelector('.carousel');
+console.log(thumbElement);
     //    -creo una variabile come segnaposto che mi servirà per spostare la classe active tra le immagini
 let activeImg = 0;
+//let shownImg = 0
     //    -ciclo per scorrere nell'array d'immagini e sostituire sequenzialmente la classe active
 
 
 for (let i = 0; i < imgList.length; i++) {
     const img = imgList[i];
     //console.log(img);
-    let imgMarkup = `<img class="${i === activeImg ? "active" : "d_none"}" src="./assets/img/${img}" alt=""></img>`;
-    //console.log(imgMarkup);
-    slider.insertAdjacentHTML('beforeend', imgMarkup);
+    let currentImgMarkup = `<img class="${i === activeImg ? "active" : "d_none"}" src="./assets/img/${img}" alt=""></img>`;
+    let shownImgMarkup = `<img class="${i === activeImg ? "shown" : "unshown"}" src="./assets/img/${img}" alt=""></img>`;
+    //console.log(shownImgMarkup);
+    slider.insertAdjacentHTML('beforeend', currentImgMarkup);
+    thumbElement.insertAdjacentHTML('beforeend', shownImgMarkup);
 }
+console.log(thumbElement);
 
 // - aggiungo gli eventi ai click dei due bottoni (avanti e indietro)
 const up = document.querySelector('.up');
@@ -39,6 +45,21 @@ up.addEventListener('click', function(){
     allImg[activeImg].classList.add("active");
     allImg[activeImg].classList.remove('d_none');
 
+    let thumbImg = document.querySelector('img.shown');
+    //console.log(imgElement);
+    thumbImg.classList.remove("shown");
+    thumbImg.classList.add("unshown");
+    
+    
+    //console.log(imgElement.classList);
+    let allThumbImg = document.querySelectorAll('.carousel img');
+    //console.log(allThumbImg);
+    allThumbImg[activeImg].classList.add("shown");
+    allThumbImg[activeImg].classList.remove("unshown");
+    
+
+
+
 })
 
 down.addEventListener('click', function(){
@@ -57,4 +78,15 @@ down.addEventListener('click', function(){
     //console.log(allImg);
     allImg[activeImg].classList.add("active");
     allImg[activeImg].classList.remove('d_none');
+    let thumbImg = document.querySelector('img.shown');
+    //console.log(imgElement);
+    thumbImg.classList.remove("shown");
+    thumbImg.classList.add("unshown");
+    
+    
+    //console.log(imgElement.classList);
+    let allThumbImg = document.querySelectorAll('.carousel img');
+    //console.log(allThumbImg);
+    allThumbImg[activeImg].classList.add("shown");
+    allThumbImg[activeImg].classList.remove("unshown");
 })
